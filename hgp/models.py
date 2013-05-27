89420d8ebae055262f22cff39c16de821a38d892
Joaquín Sorianello <joac@joac.com.ar>
2013-05-27 17:12:02 -0300

    Agrego Modelo de video

--- hgp/models.py
+++ hgp/models.py
@@ -5,17 +5,32 @@ import datetime
 metadata.bind = "mysql://c0hgp_client:test@localhost/c0sitio_hgp_db"
 session.autocommit = True
 class Photo(Entity):
-    
+
     title = Field(Unicode(60))
-    timestamp = Field(Integer)
     description = Field(UnicodeText)
     filehash = Field(Unicode(40))
-    timestamp = Field(DateTime, default=datetime.datetime.now) 
+    peso  = Field(Integer)
+    timestamp = Field(DateTime, default=datetime.datetime.now)
     tags = ManyToMany('Tag')
-    
+
     def __repr__(self):
-        return '<Foto "%s" de  galeria "%s" subida el %s>' % (self.title, self.gallery, self.timestamp)
-    
+        return '<Foto "%s" subida el %s>' % (self.title, self.timestamp)
+
+    def get_tag_string(self):
+        return ', '.join([t.name for t in self.tags])
+
+class Video(Entity):
+
+    title = Field(Unicode(60))
+    description = Field(UnicodeText)
+    peso  = Field(Integer)
+    url = Field(Unicode(255))
+    timestamp = Field(DateTime, default=datetime.datetime.now)
+    tags = ManyToMany('Tag')
+
+    def __repr__(self):
+        return '<Video "%s" subida el %s>' % (self.title, self.timestamp)
+
     def get_tag_string(self):
         return ', '.join([t.name for t in self.tags])
 
